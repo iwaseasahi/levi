8f3f1342bda70fdd62f27de5cfa412af520d1956
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2026-09-01 08:51:38 +0900

    feat: add sidebar to slide details (#423)

--- src/app/slides/(manage)/new/page.tsx
+++ src/app/slides/(manage)/new/page.tsx
@@ -1,7 +1,12 @@
 import { requireChurchPageAccess } from "@/app/church/require-church-page-access";
 import { SlideEditor } from "@/app/slides/slide-editor";
+import { SlideManageShell } from "@/app/slides/slide-manage-shell";
 
 export default async function NewSlidePage() {
   await requireChurchPageAccess();
-  return <SlideEditor />;
+  return (
+    <SlideManageShell>
+      <SlideEditor />
+    </SlideManageShell>
+  );
 }
